97174ddffa574982e1bb032583dc802bb781b02a
Pierre Berthe <pierre1.berthe@epitech.eu>
2023-12-04 11:26:24 +0100

    Correct animation

--- script/player.gd
+++ script/player.gd
@@ -5,7 +5,7 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 var anim_vec: Vector2
 @onready var anim_player = $Barbarian/AnimationPlayer
-@onready var tree = $AnimationTree
+@onready var anim_tree = $AnimationTree
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -17,12 +17,18 @@ func _physics_process(delta):
 
 	# Start animation
 	if Input.is_action_just_pressed("attack"):
-		anim_player.play("1H_Melee_Attack_Slice_Horizontal")
+		anim_tree.active = false
+		anim_player.play("1H_Melee_Attack_Chop")
+		await anim_player.animation_finished
+		anim_tree.active = true
 
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
+		anim_tree.active = false
 		velocity.y += JUMP_VELOCITY
 		anim_player.play("Jump_Full_Short")
+		await anim_player.animation_finished
+		anim_tree.active = true
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -39,6 +45,6 @@ func _physics_process(delta):
 	anim_vec.y = direction.z
 	anim_vec = anim_vec.normalized()
 
-	tree["parameters/blend_position"] = anim_vec
+	anim_tree["parameters/blend_position"] = anim_vec
 
 	move_and_slide()
